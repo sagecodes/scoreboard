30a0a7ece24189e42bf2eac7b5e24209df4b35f9
Sage Elliott <wildtroll@gmail.com>
2016-09-18 18:54:31 -0700

    Initial Commit | Basic recat component

---
+++ app.jsx
@@ -0,0 +1,10 @@
+function Application() {
+  return (
+    <div>
+      <h1>Hello from React</h1>
+      <p>I was rendered from the Application component!</p>
+    </div>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById('container'));
